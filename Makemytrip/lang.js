f21990e965f8d898364c0a6b8b30b21da40d5a96
Dev2139 <dev.patel.codinggita@gmail.com>
2024-11-06 11:58:57 +0530

    websites

---
+++ Makemytrip/lang.js
@@ -0,0 +1,6 @@
+function changeLanguage() {
+    const language = document.getElementById("languages").value;
+    // Logic to change the website language based on the selected value
+    console.log("Selected language:", language);
+    // Here you can implement the logic to load the respective language content
+}
